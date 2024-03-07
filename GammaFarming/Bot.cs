5f220cbbadb8386aa0b8c90298a1c2027e461f73
Christian <114274872+CristianLaimon@users.noreply.github.com>
2024-03-07 14:51:07 -0600

    Add project files.

---
+++ GammaFarming/Bot.cs
@@ -0,0 +1,131 @@
+﻿using Newtonsoft.Json;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using GammaFarming.Model;
+
+namespace GammaFarming
+{
+    internal class Bot
+    {
+        private EdgeDriver window;
+        private List<string> tabs;
+        private User[] dummyData;
+        private string tempEmail;
+        private List<string> landings;
+
+        public Bot()
+        {
+            dummyData = JsonConvert.DeserializeObject<User[]>(File.ReadAllText("./users.json"));
+            tabs = new();
+            tempEmail = "";
+            landings = new List<string>() { "landing", "landing.", "landing..", "landing..." };
+        }
+
+        public void FarmCredits(string link, byte times)
+        {
+            for(byte i = 0; i < times; i++)
+            {
+                Get200Credits(link);
+            }
+        }
+
+        private void Get200Credits(string referrallink)
+        {
+            window = new();
+
+            //-------TempMail-------- (Tab 1)
+            window.Navigate().GoToUrl(@"https://emailtemp.org/es");
+            tabs.Add(window.CurrentWindowHandle);
+            this.tempEmail = QuerySelector("input.custom-email-input").GetAttribute("value");
+
+            // Esperar hasta que el valor del correo temporal no sea "landing"
+            while (landings.Contains(tempEmail))
+            {
+                this.tempEmail = QuerySelector("input.custom-email-input").GetAttribute("value");
+                Thread.Sleep(100); // Esperar un momento antes de volver a verificar
+            }
+
+            //--------------Gama Tab-------------- (Tab 2)
+            tabs.Add(CreateAndGoToNewTab());
+            window.Navigate().GoToUrl(referrallink);
+            QuerySelector("input#email").SendKeys(tempEmail);
+            QuerySelector("button.chakra-button.css-500mhu").Click();  //ContinueButton click
+
+            //try //                        I cant bypass CAPTCHA GODdammit :)
+            //{
+            //    var waiting = new WebDriverWait(window, TimeSpan.FromSeconds(5));
+            //    waiting.Until(ExpectedConditions.ElementToBeClickable(By.CssSelector("div[role=\"presentation\"]")));
+            //}
+            //catch
+            //{
+            //}
+
+            //-------------------TempMail--------------------- (Tab 2)
+            //    Wait and click received email from gamma
+            window.SwitchTo().Window(tabs.First());
+            QuerySelector("i.fas.fa-chevron-right").Click(); //Click on emaildeliveredDiv (>)
+            string hrefRealEmail = QuerySelector("div.content iframe").GetAttribute("src");
+
+            //----------Verification Email tab ---------- (Tab 3)
+            tabs.Add(CreateAndGoToNewTab());
+            window.Navigate().GoToUrl(hrefRealEmail);
+            string signUpGammaURL = QuerySelector("a.button").GetAttribute("href");
+
+            //----------Sign Up Page GAMMA ------------- (Tab 4)
+            tabs.Add(CreateAndGoToNewTab());
+            window.Navigate().GoToUrl(signUpGammaURL);
+            var firstName = GetElementById("first name");
+            var lastName = GetElementById("last name");
+            var password = GetElementById("password");
+
+            User u = dummyData[new Random().Next(0, dummyData.Length)];
+            firstName.SendKeys(u.first_name);
+            lastName.SendKeys(u.last_name);
+            password.SendKeys(u.password);
+
+            var buttonSubmit = QuerySelector("button[type=\"submit\"]");
+            buttonSubmit.Click();
+
+            var workSpaceField = GetElementById("field-:r11:");
+            workSpaceField.SendKeys(u.workspace);
+
+            window.Quit();
+            tabs.Clear();
+        }
+
+
+
+        private IWebElement QuerySelector(string cssSelector, byte time = 50)
+        {
+            var waiting = new WebDriverWait(window, TimeSpan.FromSeconds(time));
+            return waiting.Until(ExpectedConditions.ElementIsVisible(By.CssSelector(cssSelector)));
+        }
+        private IWebElement GetElementById(string id, byte time = 50 )
+        {
+            var waiting = new WebDriverWait(window, TimeSpan.FromSeconds(time));
+            return waiting.Until(ExpectedConditions.ElementIsVisible(By.Id(id)));
+        }
+        private void TestTab()
+        {
+            var window = new EdgeDriver();
+            string firstPage = window.CurrentWindowHandle;
+            window.Navigate().GoToUrl(@"https://google.com");
+
+            window.SwitchTo().NewWindow(WindowType.Tab);
+            window.Navigate().GoToUrl(@"https://facebook.com");
+            string secondPage = window.CurrentWindowHandle;
+        }
+
+        /// <summary>
+        /// Returns id of the new window and adds a new tab to actual window
+        /// </summary>
+        /// <returns></returns>
+        private string CreateAndGoToNewTab()
+        {
+            window.SwitchTo().NewWindow(WindowType.Tab);
+            return window.CurrentWindowHandle;
+        }
+    }
+}
---
+++ GammaFarming/Program.cs
@@ -0,0 +1,50 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
+using SeleniumExtras.WaitHelpers;
+using Newtonsoft.Json;
+using GammaFarming;
+using OpenQA.Selenium.Edge;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        args = new string[] { "https://gamma.app/signup?r=zw517ayh03nhnzb", "1" }; //test, put here your referral code
+
+        if (args.Length != 2)
+        {
+            Console.WriteLine("""
+                Gamma Referral Bog Farmer
+                USAGE - gamabot [referral link (string)] [number of accounts (200credits per each) (number)
+
+                Just put your referral link as the only argument of this proyect and
+                be aware to click on captcha on sign up page. Thats the only thing
+                you need to do.
+
+            """);
+
+            Console.WriteLine(
+                "Note: The whole process should be less than 50 seconds for every" +
+                " account created, otherwise, this can break... (try again with better internet)");
+
+            Console.WriteLine(
+                "Note2: CAPTCHA can't be bypass by this bot, its the only thing you should" +
+                " click by yourself...");
+
+            Console.WriteLine("Finishing program");
+            return;
+        }
+
+        if (!int.TryParse(args[1], out int times))
+        {
+            Console.WriteLine("Second argument is not a number!!!");
+            Console.WriteLine("Finishing program");
+        }
+
+
+        //string referrallink = @"https://gamma.app/signup?r=zw517ayh03nhnzb";
+        var bot = new Bot();
+        bot.FarmCredits(link: args[0], times: byte.Parse(args[1]));
+    }
+}
+
